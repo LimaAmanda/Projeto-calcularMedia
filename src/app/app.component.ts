1505f77600858d486e96b85ac690d0709d6154e7
Amanda Cristina <amandacris_lima@outlook.com>
2022-01-23 18:07:25 -0300

    Mudança de layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   public codigoRMA : number = 123456 ;
-  public nome : string = 'Nome';
+  public nome : string = 'Amanda';
   public nota1 : number = 10;
-  public nota2 : number = 10;
+  public nota2 : number = 5;
   public nota3 : number = 5;
-  public nota4 : number = 3;
+  public nota4 : number = 10;
   public media : number = (this.nota1+this.nota2+this.nota3+this.nota4 )/4 ;
   public aprovado : boolean = true;
   public reprovado : boolean= false;
   public foto : string = 'assets/img/web.png';
-  datavalidade:string = '2021-12-30'
+  datavalidade:string = '2022-12-15'
 
   constructor()
   {
     //variavel com concatenação
-    console.log('CodigoRMA :', this.codigoRMA  = 123456 );
+
+    console.log('CodigoRMA :', this.codigoRMA  = 2550 );
     console.log('Nome :', this.nome);
     console.log('Nota1 :', this.nota1);
     console.log('Nota2 :', this.nota2);
@@ -33,7 +34,7 @@ export class AppComponent implements OnInit {
         this.media = (this.nota1 + this.nota2 + this.nota3 + this.nota4)/4;
 
         this.media
-        console.log('Sua Média :',this.media)
+        console.log('Sua Média é :',this.media)
 
         if (this.media >= 7)
         {
